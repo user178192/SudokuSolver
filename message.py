9e9df221c57f93d636954dfe77e807cbfb8a722d
user178192 <user178192@users.noreply.github.com>
2014-10-29 14:28:41 -0700

    Update message.py

--- message.py
+++ message.py
@@ -1,6 +1,6 @@
 exam_msg = "Please upload .cvs file. The Input format should be 9 x 9 grid with values between 0 and 9"
 usage_msg = "Usage: python solver.py input_filename output_filename"
-invaild_msg = "Failed for invalid Sudoku input, not standard sudoku version \
-the. Input format should 9 x 9 grid with values between 0 and 9"
+invaild_msg = "Failed for invalid Sudoku input, not standard sudoku version. \
+the input format should 9 x 9 grid with values between 0 and 9"
 suc_msg = "Success"
 nosol_msg = "Failed for invalid Sudoku input, this Sudoku can't get solution"
